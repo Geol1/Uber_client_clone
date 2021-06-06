57f426f57ba5c62a67d34db3bd7e528212a9bbcb
Oldrich <oldrichngnitedem4@gmail.com>
2021-06-06 02:08:21 +0200

    Ngnitedem commit
    mapping V1

--- index.js
+++ index.js
@@ -2,10 +2,8 @@
  * @format
  */
 
-// import "./i18n";
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import 'react-native-gesture-handler';
 
 AppRegistry.registerComponent(appName, () => App);
